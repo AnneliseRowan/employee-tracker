1cdea2bb922ca9fcef730c527323e235c2cc64d3
Annélise Rowan <40217057+AnneliseRowan@users.noreply.github.com>
2021-06-14 12:15:10 -0400

    trying to connect the functions

--- develop/employeeBasic.js
+++ develop/employeeBasic.js
@@ -42,6 +42,41 @@ const start = () => {
       }
 };
 
+const viewAll = () => {
+  connection.query(
+    'SELECT * FROM Department', (err, res) => {
+      if(err) {
+        console.log(`Ahhhhh : `, err); 
+      }
+      console.table(res); 
+      start(); 
+    }
+  )
+};
+
+const viewByDepartment = () => {
+  connection.query(
+    'SELECT * FROM Department', (err, res) => {
+      if(err) {
+        console.log(`Ahhhhh : `, err); 
+      }
+      console.table(res); 
+      start(); 
+    }
+  )
+};
+
+const viewByManager = () => {
+  connection.query(
+    'SELECT * FROM employee_role', (err, res) => {
+      if(err) {
+        console.log(`Ahhhhh : `, err); 
+      }
+      console.table(res); 
+      start(); 
+    }
+  )
+};
 
 const addEmployee = () => {
   inquirer
@@ -60,97 +95,83 @@ const addEmployee = () => {
         name: 'role',
         type: 'input',
         message: `What is the employee's role?`,
-        choices: ['Sales Lead', 'Salesperson', 'Lead Engineer', 'Software Engineer', 'Account Manager', 'Accountant', 'Legal Team Lead'],
+        choices: ['Sales Lead', 'Salesperson', 'Lead Engineer', 'Software Engineer', 'Account Manager', 'Accountant', 'Legal Team Lead'], // MAYBE EASIER WAY TO REUSE?
       },
       {
         name: 'manager',
         type: 'input',
         message: `Who is the employee's manager?`,
-        choices: ['', '', '', '', ''], // FIGURE OUT A WAY TO CALL THE MANAGERS FROM DB
+        choices: selectingManager(), 
       },
     ])
     .then((answer) => {
-      // when finished prompting, insert a new item into the db with that info
+      let roleId = answer.role; 
+      let managerId = answer.manager; 
+
       connection.query(
-        'INSERT INTO auctions SET ?',
-        // QUESTION: What does the || 0 do?
+        'INSERT INTO employee SET ?',
         {
           first_name: answer.firstName,
           last_name: answer.lastName,
-          role_id: answer.role,
-          manager_id: answer.manager,
+          role_id: roleId,
+          manager_id: managerId,
         },
         (err) => {
           if (err) throw err;
+          console.table(answer); 
           console.log(`You've successfully added an employee!!`);
-          // re-prompt the user for if they want to bid or post
           start();
         }
       );
     });
 };
 
-const bidAuction = () => {
-  // query the database for all items being auctioned
-  connection.query('SELECT * FROM auctions', (err, results) => {
-    if (err) throw err;
-    // once you have the items, prompt the user for which they'd like to bid on
-    inquirer
-      .prompt([
-        {
-          name: 'choice',
-          type: 'rawlist',
-          choices() {
-            const choiceArray = [];
-            results.forEach(({ item_name }) => {
-              choiceArray.push(item_name);
-            });
-            return choiceArray;
-          },
-          message: 'What auction would you like to place a bid in?',
-        },
-        {
-          name: 'bid',
-          type: 'input',
-          message: 'How much would you like to bid?',
-        },
-      ])
-      .then((answer) => {
-        // get the information of the chosen item
-        let chosenItem;
-        results.forEach((item) => {
-          if (item.item_name === answer.choice) {
-            chosenItem = item;
-          }
-        });
 
-        // determine if bid was high enough
-        if (chosenItem.highest_bid < parseInt(answer.bid)) {
-          // bid was high enough, so update db, let the user know, and start over
-          connection.query(
-            'UPDATE auctions SET ? WHERE ?',
-            [
-              {
-                highest_bid: answer.bid,
-              },
-              {
-                id: chosenItem.id,
-              },
-            ],
-            (error) => {
-              if (error) throw err;
-              console.log('Bid placed successfully!');
-              start();
-            }
-          );
-        } else {
-          // bid wasn't high enough, so apologize and start over
-          console.log('Your bid was too low. Try again...');
-          start();
+const removeEmployee = () => {
+  connection.promise().query("SELECT * FROM employee")
+    .then((res) => {
+      return res[0].map(employee => {
+        return {
+          name: employee.first_name,
+          value: employee.id
         }
-      });
-  });
+      })
+    .then()
+        // (err) => {
+        //   if (err) throw err;
+        //   console.table(answer); 
+        //   console.log(`You've successfully added an employee!!`);
+        //   start();
+        // }
+
+    });
 };
+
+const updateEmployeeRole = () => {
+
+}
+
+const updateManager = () => {
+
+}
+
+const selectingManager = () => {
+  return connection.prependOnceListener().query("SELECT * FROM EMPLOYEE")
+  .then(res => {
+    return res[0].map(manager => {
+      return {
+        name: `${manager.first_name} ${manager.last_name}`,
+        value: manager.id,
+      }
+    })
+  })
+}
+
+
+
+
+
+
 
 // connect to the mysql server and sql database
 connection.connect((err) => {
