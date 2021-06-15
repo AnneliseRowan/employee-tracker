c95a40e9274696ac044d1a106e679df3db47f7ac
Annélise Rowan <40217057+AnneliseRowan@users.noreply.github.com>
2021-06-15 18:40:16 -0400

    just pushing before class starts

--- develop/employeeBasic.js
+++ develop/employeeBasic.js
@@ -18,13 +18,13 @@ const start = () => {
       name: 'intro',
       type: 'list',
       message: 'What would you like to do?',
-      choices: ['View All Employees', 'View All Employees By Department', 'View All Employees By Roles', 'Add Employee', 'Remove Employee', 'Update Employee Role', 'Update Employee Manager'],
+      choices: ['View All Employees By Lastname', 'View All Employees By Department', 'View All Employees By Titles', 'Add Employee', 'Remove Employee', 'Update Employee Role', 'Update Employee Manager'],
     }).then((answer) => {
-      if (answer.intro === 'View All Employees') {
-        viewAll(); 
+      if (answer.intro === 'View All Employees By Lastname') {
+        viewAllByEmployee(); 
       } else if (answer.intro === 'View All Employees By Department') {
         viewByDepartment(); 
-      } else if (answer.intro === 'View All Employees By Roles') {
+      } else if (answer.intro === 'View All Employees By Titles') {
         viewByRoles(); 
       } else if (answer.intro === 'Add Employee') {
         addEmployee();
@@ -40,12 +40,6 @@ const start = () => {
     })  
 };
 
-/* Reference from W3Schools
-SELECT column_name(s)
-FROM table1
-INNER JOIN table2
-ON table1.column_name = table2.column_name;
-*/
 
 /*
 SELECT e.id, CONCAT(e.first_name, ' ', e.last_name) AS Employee, employee_role.title AS Title, department_name AS Department, employee_role.salary AS Salary, CONCAT(m.first_name, " ", m.last_name) AS Manager 
@@ -53,10 +47,10 @@ FROM employee e
 LEFT JOIN employee m ON m.id = e.manager_id JOIN employee_role ON e.role_id = employee_role.id JOIN department ON department.id = employee_role.department_id 
 */
 
-const viewAll = () => {
+const viewAllByEmployee = () => {
   console.log(`Loading all information... \n`)
   connection.query(
-    "SELECT * FROM employee INNER JOIN employee_role ON employee.role_id = employee_role.id INNER JOIN department ON employee_role.department_id = department.id", 
+    "SELECT e.id, CONCAT(e.first_name, ' ', e.last_name) AS Employee, employee_role.title AS Title, department_name AS Department, employee_role.salary AS Salary, CONCAT(m.first_name,' ', m.last_name) AS Manager FROM employee e LEFT JOIN employee m ON m.id = e.manager_id JOIN employee_role ON e.role_id = employee_role.id JOIN department ON department.id = employee_role.department_id ORDER BY e.last_name ASC", 
     (err, res) => {
       if(err) {
         console.error(`Ahhhhh : `, err); 
@@ -81,17 +75,10 @@ const viewByDepartment = () => {
   )
 };
 
-/* Reference from W3Schools
-SELECT column_name(s)
-FROM table1
-INNER JOIN table2
-ON table1.column_name = table2.column_name;
-*/
-
 const viewByRoles = () => {
   console.log(`Loading information by roles... \n`)
   connection.query(
-    "SELECT * FROM employee_role INNER JOIN department ON employee_role.department_id",
+    "SELECT e.id, CONCAT(e.first_name, ' ', e.last_name) AS Employee, employee_role.title AS Title, department_name AS Department, employee_role.salary AS Salary, CONCAT(m.first_name,' ', m.last_name) AS Manager FROM employee e LEFT JOIN employee m ON m.id = e.manager_id JOIN employee_role ON e.role_id = employee_role.id JOIN department ON department.id = employee_role.department_id ORDER BY employee_role.title ASC",
     (err, res) => {
       if(err) {
         console.error(`Ahhhhh : `, err); 
@@ -102,7 +89,9 @@ const viewByRoles = () => {
   )
 };
 
-const addEmployee = () => {
+async function addEmployee() { //SHOULD BE AN ASYNC FUNCTION TO AWAIT??? --- trying it out
+  const managers = await updateManager(); 
+  
   inquirer
     .prompt([
       {
@@ -132,17 +121,16 @@ const addEmployee = () => {
       },
       {
         name: 'manager',
-        type: 'input',
+        type: 'list',
         message: `Who is the employee's manager?`,
-        choices: selectingManager(), 
+        choices: managers //NEED TO FIGURE OUT A WAY TO MAKE A LIST OF EMPLOYEES INTO AN ARRAY OF STRINGS CONCATTING FIRST_NAME AND LAST_NAME
       },
-    ])
-    .then((answer) => {
+    ]).then(function (answer) {
       let roleId = answer.role; 
       let managerId = answer.manager; 
 
       connection.query(
-        'INSERT INTO employee SET ?',
+        "INSERT INTO employee", // SOMETHING WRONG HERE -- PULLS THIS TABLE UP FIRST
         {
           first_name: answer.firstName,
           last_name: answer.lastName,
@@ -161,7 +149,7 @@ const addEmployee = () => {
 
 
 const removeEmployee = () => {
-  connection.query("SELECT * FROM employee") //should maybe use connection.promise().query()????
+  connection.promise().query("SELECT * FROM employee") //should maybe use connection.promise().query()????
     .then((res) => {
       return res[0].map(employee => {
         return {
@@ -184,25 +172,33 @@ const updateEmployeeRole = () => {
 
 }
 
-const updateManager = () => {
-  connection.query("SELECT * FROM employee", (err, res) => { //maybe should use connection.promisefy()???
-    console.table(res); 
+// const updateManager = () => {
+//   connection.query("SELECT * FROM employee", 
+//     (err, res) => { //maybe should use connection.promisefy()???
+//        console.log(res)
+//       return res[0].map(m => {
+//         return {
+//           name: `${m.first_name} ${m.last_name}`,
+//           value: m.id
+//         }
+//       })
+//     console.table(res); 
 
 
-  })
-}
-
-// const selectingManager = () => {
-//   return connection.prependOnceListener().query("SELECT * FROM EMPLOYEE")
-//   .then(res => {
-//     return res[0].map(manager => {
-//       return {
-//         name: `${manager.first_name} ${manager.last_name}`,
-//         value: manager.id,
-//       }
-//     })
 //   })
 // }
+
+const selectingManager = () => {
+  connection.promise().query("SELECT * FROM employee")
+  .then(res => {
+    return res[0].map(manager => {
+      return {
+        name: employee.first_name,
+        value: employee.id,
+      }
+    })
+  })
+}
 
 
 
